72c1de033729a815877f4469f8968656fea042d9
ninjaskawina <ninjaskawina@gmail.com>
2016-05-17 15:59:42 +0200

    Create get_humidity.py
    Gets humidity from Cracow, Warsow and Poznan and generates data in JSON syntax

---
+++ get_humidity.py
@@ -0,0 +1,20 @@
+#C:\Python\python.exe C:\Users\Ninja\Desktop\parse.py
+#62c813137a6c5df7bd8f031400c0102c
+import urllib.request
+import xml.etree.ElementTree as ET
+
+response = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Cracow&APPID=62c813137a6c5df7bd8f031400c0102c&mode=xml&units=metric").read()
+current = ET.fromstring(response)
+temp1 = current[2].attrib['value']
+response = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Warszawa%20Angeles&APPID=62c813137a6c5df7bd8f031400c0102c&mode=xml&units=metric").read()
+current = ET.fromstring(response)
+temp2 = current[2].attrib['value']
+response = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Poznan&APPID=62c813137a6c5df7bd8f031400c0102c&mode=xml&units=metric").read()
+current = ET.fromstring(response)
+temp3 = current[2].attrib['value']
+
+
+print("{\n\t\"temp1\": \"" + temp1 + "\",\n")
+print("\t\"temp2\": \"" + temp2 + "\",\n")
+print("\t\"temp3\": \"" + temp3 + "\"\n")
+print("}")
